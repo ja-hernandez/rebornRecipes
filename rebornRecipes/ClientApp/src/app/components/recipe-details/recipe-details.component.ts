533204c91aee1bffc188d7a8acd1a82403195696
Jose Hernandez <jose.a.hernandez.g@gmail.com>
2021-04-20 21:22:46 -0500

    more work on recipe list and first pass of recipe details

--- rebornRecipes/ClientApp/src/app/components/recipe-details/recipe-details.component.ts
+++ rebornRecipes/ClientApp/src/app/components/recipe-details/recipe-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from "src/app/services/recipe.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.component.css']
 })
 export class RecipeDetailsComponent implements OnInit {
+  currentRecipe: any;
+  message = '';
+  parsedIngredients: string[];
+  parsedInstructions: string[];
 
-  constructor() { }
+
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.message ='';
+    this.getRecipe(this.route.snapshot.paramMap.get('id'))
   }
 
+  getRecipe(id) {
+    this.recipeService.get(id)
+      .subscribe(
+        recipe => {
+          this.currentRecipe = recipe;
+          console.log(recipe);
+          this.parsedIngredients = recipe.ingredients.split("\n");
+          this.parsedInstructions =  recipe.instructions.split(". ")
+        },
+        error => {
+          console.log(error);
+        }
+      )
+  }
 }
